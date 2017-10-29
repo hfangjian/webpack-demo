39d9a063a2b2e13f6d63b7dd170b39f37974c5cf
hfangjian <357987103@qq.com>
2017-10-30 00:18:21 +0800

    配置代码分离环境

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-
+const webpack = require('webpack');
 module.exports = merge(common, {
   
   devtool: 'inline-source-map',
@@ -9,5 +9,7 @@ module.exports = merge(common, {
     contentBase: './dist',
     hot: true
   },
-
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 });
